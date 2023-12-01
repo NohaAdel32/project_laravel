4ceca16b15f74fb0ec57bc706f4cc931e21739f9
NohaAdel32 <love.queen1921989@gmail.com>
2023-12-01 15:37:08 +0300

    update

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,34 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//Route::fallback(function(){
+  //  return redirect('/');
+//});
+Route::get('food', function(){
+    return view('food');
+});
+Route::prefix('lar')->group(function(){
+    Route::get('/', function(){
+        return view('test');
+    });
+Route::get('test', function(){
+    return 'welcome my first laravel website';
+});
+Route::get('/test1/{id}', function($id){
+    return 'The ID is :' . $id;
+});
+Route::get('/test2/{id?}', function($id = 0){
+    return 'The ID 2 is :' . $id;
+})->whereNumber('id');
+Route::get('/test3/{name?}', function($name = null){
+    return 'The name is :' . $name;
+})->whereAlpha('name');
+
+Route::get('/test4/{id?}/{name?}', function($id = 0, $name){
+    return 'The ID 2 is :' . $id . "  ".'The name is :' . $name;
+})->where(['id' => '[0-9]+', 'name'=>'[a-zA-Z]+']);
+
+Route::get('/product/{category?}', function($cat){
+    return 'The category is :' . $cat ;
+})->whereIn('category',['pc','moile','tv']);
+});
